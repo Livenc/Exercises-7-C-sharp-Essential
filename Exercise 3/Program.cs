4070b40d0a28b53dc5b7dc6c0ae6cf1872910e2a
liven <Artemalimov333@gmail.com>
2023-03-11 13:47:26 +0200

    Add exercise 3

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -10,10 +10,25 @@
         {
             public string change;
         }
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "first";
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "first";
+        }
         static void Main(string[] args)
         {
 
-
+            MyClass myClass = new MyClass();
+            myClass.change = "second";
+            MyStruct myStruct = new MyStruct();
+            myStruct.change = "second";
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+            Console.WriteLine(myClass.change);
+            Console.WriteLine(myStruct.change);
             Console.ReadLine();
         }
     }
